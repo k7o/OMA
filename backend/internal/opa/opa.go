6e4d22bf2b567dd95dec6f3bfbf0fb65dd9fe018
Pieter van der Werk <pieter.vdwerk@gmail.com>
2024-03-17 14:24:51 +0100

    chore: restructure opa package

--- backend/internal/opa/opa.go
+++ backend/internal/opa/opa.go
@@ -46,6 +46,21 @@ func (opa *Opa) Eval(policy string, input string) (*models.EvalResult, error) {
 	return &result, nil
 }
 
+func (opa *Opa) Format(policy string) (string, error) {
+	policyFile, cleanup, err := writeBytesToFile([]byte(policy), "rego")
+	defer cleanup()
+	if err != nil {
+		return "", err
+	}
+
+	output, err := cmd(fmt.Sprintf("fmt %s", policyFile))
+	if err != nil {
+		return "", err
+	}
+
+	return string(output), nil
+}
+
 func cmd(command string) ([]byte, error) {
 	splits := strings.Split(command, " ")
 	if command == "" || len(splits) == 0 {
