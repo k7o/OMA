03478f68518514e9299ce3edb5ccdd03982fc1df
Eric Kerst <eric.kerst@vecozo.nl>
2024-10-10 18:13:51 +0200

    wip

--- internal/revision/policy_proxy_revision_repository.go
+++ internal/revision/policy_proxy_revision_repository.go
@@ -34,16 +34,20 @@ func NewPolicyProxyRevisionRepository(conf *PolicyProxyRevisionRepositoryConfig)
 }
 
 type PolicyProxyPackage struct {
-	ID          int       `json:"id"`
-	Name        string    `json:"name"`
-	Version     string    `json:"version"`
-	PackageType string    `json:"package_type"`
-	Status      string    `json:"file_name"`
-	CreatedAt   time.Time `json:"created_at"`
-	Digest      string    `json:"digest"`
+	ID        string    `json:"id"`
+	Name      string    `json:"name"`
+	Version   string    `json:"version"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+type PolicyProxyRevisionFiles []struct {
+	FileName string `json:"file_name"`
 }
 
 func (r *PolicyProxyRevisionRepository) ListRevisions() ([]models.Revision, error) {
+
+	log.Info().Msg("ListRevisions")
+
 	url := r.conf.URL
 
 	resp, err := http.Get(url)
@@ -53,9 +57,8 @@ func (r *PolicyProxyRevisionRepository) ListRevisions() ([]models.Revision, erro
 	}
 	defer resp.Body.Close()
 
-	log.Error().Msg(err.Error())
-
 	if resp.StatusCode != http.StatusOK {
+
 		body, err := io.ReadAll(resp.Body)
 		if err != nil {
 			log.Error().Err(err).Msg("error reading response body")
@@ -65,16 +68,17 @@ func (r *PolicyProxyRevisionRepository) ListRevisions() ([]models.Revision, erro
 
 	var revisions []PolicyProxyPackage
 	if err := json.NewDecoder(resp.Body).Decode(&revisions); err != nil {
+		log.Info().Msg(err.Error())
 		return nil, err
 	}
 
 	var result []models.Revision
 	for _, revision := range revisions {
 		result = append(result, models.Revision{
-			PackageId:   fmt.Sprintf("%d", revision.ID),
+			PackageId:   revision.ID,
 			Name:        revision.Name,
 			Version:     revision.Version,
-			PackageType: revision.PackageType,
+			PackageType: "policy-proxy",
 			CreatedAt:   revision.CreatedAt,
 		})
 	}
@@ -82,15 +86,26 @@ func (r *PolicyProxyRevisionRepository) ListRevisions() ([]models.Revision, erro
 }
 
 func (r *PolicyProxyRevisionRepository) ListRevisionFiles(packageId string) ([]string, error) {
+	log.Info().Msg("ListRevisionFiles")
+
 	url := fmt.Sprintf("%s/%s/package_files", r.conf.URL, packageId)
+
+	log.Info().Msg(url)
 
 	resp, err := http.Get(url)
 	if err != nil {
 		return nil, err
 	}
 
-	var files RevisionFiles
+	// bodyBytes, err := io.ReadAll(resp.Body)
+
+	// // Log the body
+	// bodyString := string(bodyBytes)
+	// log.Info().Msg(bodyString)
+
+	var files PolicyProxyRevisionFiles
 	if err := json.NewDecoder(resp.Body).Decode(&files); err != nil {
+		log.Info().Msg(err.Error())
 		return nil, err
 	}
 
@@ -103,6 +118,8 @@ func (r *PolicyProxyRevisionRepository) ListRevisionFiles(packageId string) ([]s
 }
 
 func (r *PolicyProxyRevisionRepository) DownloadRevisionById(revisionId string) (*models.Bundle, error) {
+	log.Info().Msg("DownloadRevisionById")
+
 	url := fmt.Sprintf("%s/?package_version=%s", r.conf.URL, revisionId)
 
 	resp, err := http.Get(url)
@@ -149,6 +166,8 @@ func (r *PolicyProxyRevisionRepository) DownloadRevisionById(revisionId string) 
 }
 
 func (r *PolicyProxyRevisionRepository) DownloadRevision(revision *models.Revision) (*models.Bundle, error) {
+	log.Info().Msg("DownloadRevision")
+
 	url := fmt.Sprintf("%s/%s/%s/%s/%s", r.conf.URL, revision.PackageType, revision.Name, revision.Version, revision.FileName)
 
 	resp, err := http.Get(url)
@@ -166,13 +185,15 @@ func (r *PolicyProxyRevisionRepository) DownloadRevision(revision *models.Revisi
 }
 
 func (r *PolicyProxyRevisionRepository) DownloadRevisionForPackage(packageId string, filename string) (*models.Bundle, error) {
+	log.Info().Msg("DownloadRevisionForPackage")
+
 	resp, err := http.Get(fmt.Sprintf("%s/%s", r.conf.URL, packageId))
 	if err != nil {
 		return nil, err
 	}
 	defer resp.Body.Close()
 
-	var packageInfo GitlabPackage
+	var packageInfo PolicyProxyPackage
 	if err := json.NewDecoder(resp.Body).Decode(&packageInfo); err != nil {
 		return nil, err
 	}
@@ -181,9 +202,8 @@ func (r *PolicyProxyRevisionRepository) DownloadRevisionForPackage(packageId str
 		PackageId:   packageId,
 		FileName:    filename,
 		Name:        packageInfo.Name,
-		PackageType: packageInfo.PackageType,
+		PackageType: "policy-proxy",
 		Version:     packageInfo.Version,
 		CreatedAt:   packageInfo.CreatedAt,
 	})
-
 }
